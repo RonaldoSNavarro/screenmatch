f75a2f3fbef8087833213e33377a68e84589f6bc
ronaldosnavarro <navarro.silva@terra.com.br>
2024-05-05 19:35:16 -0400

    Criação da interface Classificavel Criação da classe Episodio Criação da classe FiltroRecomendacao

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -24,5 +27,25 @@ public class Principal {
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Tempo para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar II");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
---
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+
+    private int tempoTotal = 0;
+
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
+//    public void inclui(Filme f) {
+//        this.temtoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        this.temtoTotal += s.getDuracaoEmMinutos();
+//    }
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+
+}
