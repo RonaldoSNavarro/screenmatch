608e97fee55b54cf812acdd0327160fe9195142f
ronaldosnavarro <navarro.silva@terra.com.br>
2024-05-10 22:23:44 -0400

    Implementando interfaces Comparable, Comparator e Inferface List

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,20 +4,20 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
         meuFilme.avalia(9);
-        Serie lost = new Serie("Lost", 2000);;
+        Serie lost = new Serie("Lost", 2000);
         Filme outroFilme = new Filme("Avatar II", 2023);
         outroFilme.avalia(6);
         Filme filmeDoRonaldo = new Filme("Star Wars", 1977);
         filmeDoRonaldo.avalia(10);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filmeDoRonaldo);
         lista.add(meuFilme);
         lista.add(outroFilme);
@@ -30,5 +30,17 @@ public class PrincipalComListas {
 
         }
 
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação: " + buscaPorArtista);
+        System.out.println("Lista de títulos ordenados: ");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano: " + lista);
     }
 }
